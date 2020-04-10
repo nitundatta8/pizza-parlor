323acf9ed77a36fbe9ca3a0f01495e1a89a48a38
nitunwa <nitun.2@gmail.com>
2020-04-10 14:29:16 -0700

    add dynamically pizza list in html dom

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,10 @@ function PizzaSize(size, sizePrice){
    this.sizePrice = sizePrice;
 }
  
-function Pizza(name, description){
+function Pizza(name, description, imgSrc){
    this.name = name;
    this.description = description;
+   this.imgSrc = imgSrc;
    this.toppingList = [];
    this.sizeList = [];
 }
@@ -32,32 +33,64 @@ function Store(){
    this.pizzaList = [];
 }
 
-st
-
-Store.prototype.init = function(pizza){
-
-   var pizza1 = new Pizza("Cheese Pizza", "cheese and base Tomato Sauce");
-   var pizzaSize1 = new PizzaSize("Small", 7);
-   var pizzaSize2 = new PizzaSize("Medium", 10);
-   var pizzaSize3 = new PizzaSize("Large", 14);
-   
-   pizza1.addSize(pizzaSize1);
-   pizza1.addSize(pizzaSize2);
-   pizza1.addSize(pizzaSize3);
-   
-   var store = new Store();
-   store.showPizza(pizza1);
-   this.pizzaList.push(pizza)
+Store.prototype.addPizza = function(pizza){
+    this.pizzaList.push(pizza);
 }
 
 
+Store.prototype.init = function(){
 
+   var pizza1 = new Pizza("Cheese Pizza", "cheese and base Tomato Sauce","img/pizza_1.jpg");
+   var pizzaSize1 = new PizzaSize("Small", 7);
+   var pizzaSize2 = new PizzaSize("Medium", 10);
+   var pizzaSize3 = new PizzaSize("Large", 14);
+   pizza1.addSize(pizzaSize1);
+   pizza1.addSize(pizzaSize2);
+   pizza1.addSize(pizzaSize3);
+   this.addPizza(pizza1);
+}
+
+// function displayContactDetails(addressBookToDisplay) {
+//    var contactsList = $("ul#contacts");
+//    var htmlForContactInfo = "";
+//    addressBookToDisplay.contacts.forEach(function(contact) {
+//      htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+//    });
+//    contactsList.html(htmlForContactInfo);
+//  };
+
+ function displayPizzaList(store){
+    
+   var inputList = $(".container");
+   var htmlForInfo = "";
+   store.pizzaList.forEach(function(pizza){
+      console.log(pizza.description)
+    htmlForInfo +='<div class="d-flex flex-wrap align-content-center bg-light"> '+
+                  '<div class="p-2 border">'+ pizza.name +
+                  '<div class="card" style="width: 18rem;">' +
+                     '<img class="card-img-top" src="'+pizza.imgSrc +'" alt="Card image cap">'+
+                     '<div class="card-body">'+
+                        '<p class="card-text">'+ pizza.description+'</p>'+
+                     '</div>'+
+                  '</div>'+
+                  '</div>'
+
+   });
+   inputList.append(htmlForInfo);
+ };
 
 
 
 
 // user interface logic
 $(document).ready(function(){
+   
+   var store = new Store();
+   store.init();
+   console.log(store.pizzaList);
+   displayPizzaList(store);
+
+
    $("#inputForm").submit(function(event){
       event.preventDefault();
      
