3317f6481ba386ae7a6844158638f19aa839cab2
nitunwa <nitun.2@gmail.com>
2020-04-10 11:07:45 -0700

    add Pizza,Topping,Size object in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
+// busniess logic
+function Topping(toppingName , toppingPrice){
+   this.toppingName = toppingName;
+   this.toppingPrice = toppingPrice;
+}
+var topping1 = new Topping()
+
+function PizzaSize(size, sizePrice){
+   this.size = size;
+   this.sizePrice = sizePrice;
+}
+ 
+function Pizza(name, description){
+   this.name = name;
+   this.description = description;
+   this.toppingList = [];
+   this.sizeList = [];
+}
+
+
+Pizza.prototype.addTopping = function(topping){
+  this.toppingList.push(topping);
+}
+
+Pizza.prototype.addSize = function(pizzaSize1){
+   this.sizeList.push(pizzaSize1);
+}
+
+var pizza1 = new Pizza("Cheese Pizza", "cheese and base Tomato Sauce");
+var pizzaSize1 = new PizzaSize("Small", 7);
+var pizzaSize2 = new PizzaSize("Medium", 10);
+var pizzaSize3 = new PizzaSize("Large", 14);
+
+pizza1.addSize(pizzaSize1);
+pizza1.addSize(pizzaSize2);
+pizza1.addSize(pizzaSize3);
+
+
+
+// user interface logic
 $(document).ready(function(){
    $("#inputForm").submit(function(event){
       event.preventDefault();
